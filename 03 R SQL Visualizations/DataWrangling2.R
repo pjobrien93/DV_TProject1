74f7c56ed0a3d994d440f6ddbf803193f8d2c36f
Patricia O'Brien <patriciaobrien@Patricias-MacBook-Air.local>
2015-11-12 18:26:03 -0600

    no message

--- 03 R SQL Visualizations/DataWrangling2.R
+++ 03 R SQL Visualizations/DataWrangling2.R
@@ -6,23 +6,17 @@ require("ggplot2")
 # Change the USER and PASS below to be your UTEid
 df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from RESIDENCEHALLARREST2013"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_pjo293', PASS='orcl_pjo293', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-summary(df)
-head(df)
-
-df <- df %>% group_by(INSTNM) %>% filter(LIQUOR12 != 0, DRUG12 != 0, WEAPON12 !=0) %>% filter(LIQUOR12 != "null", DRUG12 != "null", WEAPON12 != "null"); View(df);
-
-df$LIQUOR12 <- as.numeric(as.character(df$LIQUOR12))
-
-ggplot() + 
+df %>% mutate(percent_crime = (percent_men = MEN_TOTAL/TOTAL) %>% 
+  ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
-  scale_y_discrete() +
-  labs(title='Diamonds Barchart\nAVERAGE_PRICE, WINDOW_AVG_PRICE, ') +
-  labs(x=paste("Crime"), y=paste("Institution Name")) +
+  scale_y_continuous() +
+  labs(title='Titanic') +
+  labs(x="Percent Men", y=paste("Crime Rate")) +
   layer(data=df, 
-        mapping=aes(x=as.numeric(as.character(LIQUOR12)), y=INSTNM), 
+        mapping=aes(x=as.numeric(as.character(LIQUOR12)), y=as.numeric(as.character(percent_crime))), 
         stat="identity", 
         stat_params=list(), 
-        geom="bar",
-        position=position_identity()
+        geom="point",
+        geom_params=list()
   )
