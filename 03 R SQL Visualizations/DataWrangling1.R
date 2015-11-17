8ca52e2053cd3f54a229a810f15b466fa9faa71a
Patricia O'Brien <patriciaobrien@wireless-10-147-123-107.public.utexas.edu>
2015-11-17 16:51:07 -0600

    no message

--- 03 R SQL Visualizations/DataWrangling1.R
+++ 03 R SQL Visualizations/DataWrangling1.R
@@ -3,6 +3,7 @@ require("RCurl")
 require("dplyr")
 require("ggplot2")
 require(dplyr)
+require(reshape2)
 
 # Change the USER and PASS below to be your UTEid
 df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select INSTNM, LIQUOR12, DRUG12, WEAPON12 from RESIDENCEHALLARREST2013"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_pjo293', PASS='orcl_pjo293', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
@@ -10,7 +11,7 @@ df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/nati
 summary(df)
 head(df)
 
-df <- df %>% group_by(INSTNM) %>% filter(LIQUOR12 != 0, DRUG12 != 0, WEAPON12 !=0) %>% filter(LIQUOR12 != "null", DRUG12 != "null", WEAPON12 != "null"); View(df);
+df <- df %>% group_by(INSTNM) %>% filter(LIQUOR12 != 0, DRUG12 != 0, WEAPON12 !=0) %>% filter(LIQUOR12 != "null", DRUG12 != "null", WEAPON12 != "null")
 
 df$LIQUOR12 <- as.numeric(as.character(df$LIQUOR12))
 df$DRUG12 <- as.numeric(as.character(df$DRUG12))
